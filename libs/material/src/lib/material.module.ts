c107aba98c9429c4e9b29ea85c2ad9ea548c333d
Christodoulos Fragkoudakis <christodoulos.fragkoudakis@gmail.com>
2022-11-15 20:27:33 +0200

    material lib

--- libs/material/src/lib/material.module.ts
+++ libs/material/src/lib/material.module.ts
@@ -5,7 +5,7 @@ import { MatInputModule } from '@angular/material/input';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { MatButtonToggleModule } from '@angular/material/button-toggle';
-import { MatSidenavModule, MatSidenav } from '@angular/material/sidenav';
+import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatListModule } from '@angular/material/list';
 import { MatIconModule } from '@angular/material/icon';
 import { MatToolbarModule } from '@angular/material/toolbar';
